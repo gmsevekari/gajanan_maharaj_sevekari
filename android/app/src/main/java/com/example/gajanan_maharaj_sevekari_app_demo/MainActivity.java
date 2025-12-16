51408f3bddc1bd3fb0a0696c9f251197001a0c3f
Abhishek <abhishekkulkarni@Abhisheks-MacBook-Air.local>
2025-12-15 22:50:16 -0800

    Added flutter_launcher_icons and ios app support.

--- android/app/src/main/java/com/example/gajanan_maharaj_sevekari_app_demo/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.gajanan_maharaj_sevekari_app_demo;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/gajanan_maharaj_sevekari_app_demo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gajanan_maharaj_sevekari_app_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
