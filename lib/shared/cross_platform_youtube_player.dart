52bc1eee7d44d5080ba84b0d2f6068f08feb53cf
Abhishek Kulkarni <kulkarniaabhishek@gmail.com>
2026-06-17 09:45:02 -0700

    fix: resolve layout constraints crash in youtube player on web

--- lib/shared/cross_platform_youtube_player.dart
+++ lib/shared/cross_platform_youtube_player.dart
@@ -25,66 +25,65 @@ class CrossPlatformYoutubePlayer extends StatefulWidget {
 
 class _CrossPlatformYoutubePlayerState
     extends State<CrossPlatformYoutubePlayer> {
-  mobile_player.YoutubePlayerController? _mobileController;
-  web_player.YoutubePlayerController? _webController;
+  web_player.YoutubePlayerController? _controller;
 
   @override
   void initState() {
     super.initState();
-    if (kIsWeb) {
-      _webController = web_player.YoutubePlayerController.fromVideoId(
-        videoId: widget.videoId,
-        autoPlay: widget.autoPlay,
-        params: const web_player.YoutubePlayerParams(
-          showFullscreenButton: true,
-          mute: false,
-        ),
-      );
-      _webController!.listen((event) {
-        if (event.playerState == web_player.PlayerState.ended) {
-          widget.onEnded?.call();
-        }
-      });
-    } else {
-      _mobileController = mobile_player.YoutubePlayerController(
-        initialVideoId: widget.videoId,
-        flags: mobile_player.YoutubePlayerFlags(autoPlay: widget.autoPlay),
-      );
-      _mobileController!.addListener(() {
-        if (_mobileController!.value.playerState ==
-            mobile_player.PlayerState.ended) {
-          widget.onEnded?.call();
-        }
-      });
-    }
+    _controller = web_player.YoutubePlayerController.fromVideoId(
+      videoId: widget.videoId,
+      autoPlay: widget.autoPlay,
+      params: const web_player.YoutubePlayerParams(
+        showFullscreenButton: true,
+        mute: false,
+      ),
+    );
+    _controller!.listen((event) {
+      if (event.playerState == web_player.PlayerState.ended) {
+        widget.onEnded?.call();
+      }
+    });
   }
 
   @override
   void dispose() {
-    _mobileController?.dispose();
-    _webController?.close();
+    _controller?.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return kIsWeb
-        ? web_player.YoutubePlayer(controller: _webController!)
-        : mobile_player.YoutubePlayer(
-            controller: _mobileController!,
-            bottomActions: [
-              mobile_player.CurrentPosition(),
-              mobile_player.ProgressBar(isExpanded: true),
-              mobile_player.RemainingDuration(),
-              mobile_player.PlaybackSpeedButton(),
-              IconButton(
-                icon: Icon(
+    Widget playerWidget = kIsWeb
+        ? web_player.YoutubePlayer(controller: _controller!)
+        : mobile_player.YoutubePlayer(controller: _controller!);
+
+    if (widget.onLaunchYoutube != null) {
+      playerWidget = Stack(
+        children: [
+          playerWidget,
+          Positioned(
+            top: 8,
+            right: 8,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                icon: const Icon(
                   Icons.open_in_new,
-                  color: Theme.of(context).colorScheme.onPrimary,
+                  color: Colors.white,
+                  size: 20,
                 ),
+                tooltip: 'Open in YouTube',
                 onPressed: widget.onLaunchYoutube,
               ),
-            ],
-          );
+            ),
+          ),
+        ],
+      );
+    }
+
+    return playerWidget;
   }
 }
